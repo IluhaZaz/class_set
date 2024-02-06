26cd66650ec7748a0f4fc250ffe64dbc9992fd58
Ilya Zazvonov <133998226+IluhaZaz@users.noreply.github.com>
2024-02-06 16:04:07 +0300

    закончена предварительная обработка данных в классе

--- set.cpp
+++ set.cpp
@@ -1,14 +1,58 @@
 #pragma once
 #include <algorithm> 
+#include <iostream>
+
+struct Node {
+	int _val;
+	Node* _left;
+	Node* _right;
+
+	Node(int val) :_val(val) {};
+	Node(int val, Node* left, Node* right) :_val(val), _left(left), _right(right) {};
+};
+
+int compare(const void* x1, const void* x2)
+{
+	return (*(int*)x1 - *(int*)x2);             
+}
+
+size_t remove_duplicates(int* data, size_t len) {
+	for (int i = 0; i < len; i++) {
+		int j = i;
+		while (data[i] == data[j + 1]) {
+			j++;
+		}
+		for (int k = i + 1; k < len; k++) {
+			data[k] = data[k + (j - i)];
+		}
+		len -= (j - i);
+	}
+	return len;
+}
 
 class Set {
 private:
-	int* _data;
+	Node* _root;
 	size_t _len;
 public:
-	Set() : _data(nullptr), _len(0) {};
-	Set(const int& data, size_t len) {
+	Set() : _root(nullptr), _len(0) {};
+	Set(const int* data, size_t len) {
 		int* buf = new int[len];
+		for (int i = 0; i < len; i++) {
+			buf[i] = data[i];
+		}
+		qsort(buf, len, sizeof(int), compare);
+		
+		len = remove_duplicates(buf, len);
+		
+
+		int* set = new int[len];
+		for (int i = 0; i < len; i++) {
+			set[i] = buf[i];
+		}
+
+		delete[] buf;
 		
 	}
 };
+
---
+++ set_tests.cpp
@@ -0,0 +1,6 @@
+#include "set.cpp"
+
+void main() {
+	int* d = new int[9] { 1, 2, 2, 2, 3,2,1,4 ,4 };
+	Set s(d, 9);
+}
