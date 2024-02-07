7eb236301fe8a582f55d41a41e370fb9a36da6b6
Ilya Zazvonov <133998226+IluhaZaz@users.noreply.github.com>
2024-02-07 11:01:46 +0300

    определен метод print, добавлены тесты

--- set.cpp
+++ set.cpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <algorithm> 
 #include <iostream>
+#define max(a,b) (((a) > (b)) ? (a) : (b))
 
 struct Node {
 	int _val;
@@ -89,15 +90,42 @@ void remove_nodes(Node* root) {
 	delete root;
 }
 
-int* get_n_nodes(Node* root, int n) {
 
+int help_print(Node* root, int raw, int curr) {
+
+	if (root == nullptr) {
+		return 0;
+	}
+	if (curr > raw) {
+		return 0;
+	}
+	if (curr + 1 == raw) {
+		if (root->_left)
+		{
+			std::cout << root->_left->_val << " ";
+		}
+		else
+		{
+			std::cout << "x ";
+		}
+		if (root->_right)
+		{
+			std::cout << root->_right->_val << " ";
+		}
+		else
+		{
+			std::cout << "x ";
+		}
+		return 1;
+	}
+	return help_print(root->_left, raw, curr + 1) + help_print(root->_right, raw, curr + 1);
 }
 
 class Set {
 private:
 	Node* _root;
-	size_t _len;
 public:
+	size_t _len;
 	Set() : _root(nullptr), _len(0) {};
 	Set(const int* data, size_t len) {
 		int* buf = new int[len];
@@ -149,9 +177,40 @@ public:
 		remove_nodes(_root);
 	}
 
+	int* get_array() {
+		int* buf = new int[_len];
+		get_data(_root, buf);
+		buf -= _len;
+		qsort(buf, _len, sizeof(int), compare);
+		return buf;
+	}
+
+	int help_get_height(Node* root, int lvl) {
+		if (root == nullptr) {
+			return lvl - 1;
+		}
+		return max(help_get_height(root->_left, lvl + 1), help_get_height(root->_right, lvl + 1));
+
+	}
+
+	int get_height() {
+		return help_get_height(_root, 1);
+	}
 
 	void print() {
-		
+		int printed = 0;
+		int n = 2;
+		int height = this->get_height();
+		for (int i = 1; i < height; i++) {
+			std::cout << " ";
+		}
+		std::cout << _root->_val << " " << std::endl;
+		do {
+			for (int i = n; i < height; i++) {
+				std::cout << " ";
+			}
+			n++;
+		} while (help_print(_root, n - 1, 1) && std::cout << std::endl);
 	}
 
 };
--- set_tests.cpp
+++ set_tests.cpp
@@ -1,6 +1,38 @@
 #include "set.cpp"
+#include <gtest/gtest.h>
 
-void main() {
-	int* d = new int[9] { 1, 2, 2, 2, 3,2,1,4 ,4 };
-	Set s(d, 9);
+
+TEST(SetTests, ConstructorCheck) {
+	int* array = new int[12] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+	Set s(array, 12);
+	int* data = s.get_array();
+	for (int i = 0; i < s._len; i++) {
+		EXPECT_EQ(array[i], data[i]);
+	}	
+}
+
+TEST(SetTests, ConstructorCheck2) {
+	int* array = new int[10] {2,2,1,3,4,5,4,6,7,7};
+	int* unique_array = new int[7] {1, 2, 3, 4, 5, 6, 7};
+	Set s(array, 10);
+	int* data = s.get_array();
+	for (int i = 0; i < s._len; i++) {
+		EXPECT_EQ(unique_array[i], data[i]);
+	}
+}
+
+TEST(SetTests, CopyConstructorCheck) {
+	int* array = new int[12] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+	Set s(array, 12);
+	Set s2(s);
+	int* data2 = s2.get_array();
+	for (int i = 0; i < s._len; i++) {
+		EXPECT_EQ(array[i], data2[i]);
+	}
+}
+
+TEST(SetTests, PrintCheck) {
+	int* array = new int[12] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+	Set s(array, 12);
+	s.print();
 }
